7d4583c51f6f13536beb1780a8f2cf40bf914c3e
rlfrkdms1 <rlfrkdms1@naver.com>
2024-08-07 15:40:58 +0900

    [feat/#9]: domain - 통계 추가
    - ERD와 달리 time들을 모두 int로 변경
    - nullable의 경우 개발할 때 추후 변경될 가능성 있음

---
+++ src/main/java/com/woohaengshi/backend/domain/Statistics.java
@@ -0,0 +1,45 @@
+package com.woohaengshi.backend.domain;
+
+import com.woohaengshi.backend.domain.member.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Statistics {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "daily_time")
+    private int monthlyTime;
+
+    @Column(name = "daily_time")
+    private int weeklyTime;
+
+    @Column(name = "daily_time")
+    private int dailyTime;
+
+    @JoinColumn(name = "member_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    protected Statistics() {
+    }
+
+    public Statistics(Long id, int monthlyTime, int weeklyTime, int dailyTime, Member member) {
+        this.id = id;
+        this.monthlyTime = monthlyTime;
+        this.weeklyTime = weeklyTime;
+        this.dailyTime = dailyTime;
+        this.member = member;
+    }
+}
