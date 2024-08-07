458eca19b5091eb718ac77f56c4477f840870033
rlfrkdms1 <rlfrkdms1@naver.com>
2024-08-07 17:44:27 +0900

    [feat/#4]: 공부기록이 존재하면 저장하고 아니라면 업데이트 하는 기능, 과목 저장 기능 추가
    - 해당 날짜에 이미 공부한 기록이 존재하면, time을 더해줌
    - 해당 날짜에 공부 기록 없다면, 새로 생성 후 저장
    - 과목들 저장

--- src/main/java/com/woohaengshi/backend/service/StudyRecordService.java
+++ src/main/java/com/woohaengshi/backend/service/StudyRecordService.java
@@ -1,17 +1,63 @@
 package com.woohaengshi.backend.service;
 
+import com.woohaengshi.backend.SaveRecordRequest;
+import com.woohaengshi.backend.domain.StudyRecord;
+import com.woohaengshi.backend.domain.Subject;
 import com.woohaengshi.backend.domain.member.Member;
 import com.woohaengshi.backend.exception.ErrorCode;
 import com.woohaengshi.backend.exception.WoohaengshiException;
 import com.woohaengshi.backend.repository.MemberRepository;
+import com.woohaengshi.backend.repository.StudyRecordRepository;
+import com.woohaengshi.backend.repository.SubjectRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class StudyRecordService {
 
     private final MemberRepository memberRepository;
+    private final StudyRecordRepository studyRecordRepository;
+    private final SubjectRepository subjectRepository;
+
+    @Transactional
+    public void save(SaveRecordRequest request, Long memberId) {
+        Optional<StudyRecord> optionalStudyRecord =
+                studyRecordRepository.findByDateAndMemberId(request.getDate(), memberId);
+        StudyRecord studyRecord = saveStudyRecord(request, memberId, optionalStudyRecord);
+        saveSubjects(request.getSubjects(), studyRecord);
+    }
+
+    private StudyRecord saveStudyRecord(
+            SaveRecordRequest request, Long memberId, Optional<StudyRecord> optionalStudyRecord) {
+        if (optionalStudyRecord.isPresent()) {
+            StudyRecord studyRecord = optionalStudyRecord.get();
+            studyRecord.updateTime(request.getTime());
+            return studyRecord;
+        }
+        return createStudyRecord(request, findMemberById(memberId));
+    }
+
+    private void saveSubjects(List<String> subjects, StudyRecord studyRecord) {
+        subjects.forEach(subject -> subjectRepository.save(createSubject(studyRecord, subject)));
+    }
+
+    private StudyRecord createStudyRecord(SaveRecordRequest request, Member member) {
+        return StudyRecord.builder()
+                .date(request.getDate())
+                .time(request.getTime())
+                .member(member)
+                .build();
+    }
+
+    private Subject createSubject(StudyRecord studyRecord, String subject) {
+        return Subject.builder().name(subject).studyRecord(studyRecord).build();
+    }
 
     private Member findMemberById(Long memberId) {
         return memberRepository
