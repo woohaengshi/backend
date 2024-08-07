13039e5a027513814b294b351f649000aec7f09a
rlfrkdms1 <rlfrkdms1@naver.com>
2024-08-07 15:38:27 +0900

    [feat/#9]: CreatedDate, LastModifiedDate를 사용하기 위한 설정 추가

--- src/main/java/com/woohaengshi/backend/BackendApplication.java
+++ src/main/java/com/woohaengshi/backend/BackendApplication.java
@@ -2,7 +2,9 @@ package com.woohaengshi.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BackendApplication {
 
