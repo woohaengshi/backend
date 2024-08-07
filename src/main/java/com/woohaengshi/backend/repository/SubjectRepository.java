2f7708c9886b5b3433efa62d05426f038402b2d3
rlfrkdms1 <rlfrkdms1@naver.com>
2024-08-07 17:13:30 +0900

    [feat/#4]: subject repository 추가

---
+++ src/main/java/com/woohaengshi/backend/repository/SubjectRepository.java
@@ -0,0 +1,6 @@
+package com.woohaengshi.backend.repository;
+
+import com.woohaengshi.backend.domain.Subject;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubjectRepository extends JpaRepository<Subject, Long> {}
