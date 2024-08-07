e6f1799ee2c7edbab82f936143747419b165b262
rlfrkdms1 <rlfrkdms1@naver.com>
2024-08-07 16:12:29 +0900

    [feat/#4]: member, studyRecord repository 추가

---
+++ src/main/java/com/woohaengshi/backend/repository/StudyRecordRepository.java
@@ -0,0 +1,6 @@
+package com.woohaengshi.backend.repository;
+
+import com.woohaengshi.backend.domain.StudyRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudyRecordRepository extends JpaRepository<StudyRecord, Long> {}
