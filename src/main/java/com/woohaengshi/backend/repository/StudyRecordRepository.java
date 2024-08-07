0604178629cead21342aefa4badc716b27d6b5d2
rlfrkdms1 <rlfrkdms1@naver.com>
2024-08-07 17:13:16 +0900

    [feat/#4]: date와 memberId로 공부 기록을 조회하는 기능 추가

--- src/main/java/com/woohaengshi/backend/repository/StudyRecordRepository.java
+++ src/main/java/com/woohaengshi/backend/repository/StudyRecordRepository.java
@@ -3,4 +3,9 @@ package com.woohaengshi.backend.repository;
 import com.woohaengshi.backend.domain.StudyRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StudyRecordRepository extends JpaRepository<StudyRecord, Long> {}
+import java.time.LocalDate;
+import java.util.Optional;
+
+public interface StudyRecordRepository extends JpaRepository<StudyRecord, Long> {
+    Optional<StudyRecord> findByDateAndMemberId(LocalDate date, Long memberId);
+}
