3b30db2d75302a127eddaf2ef04568f0421c46b6
rlfrkdms1 <rlfrkdms1@naver.com>
2024-08-07 15:41:18 +0900

    [feat/#9]: domain - 공부 기록 추가

---
+++ src/main/java/com/woohaengshi/backend/domain/StudyRecord.java
@@ -0,0 +1,37 @@
+package com.woohaengshi.backend.domain;
+
+import com.woohaengshi.backend.domain.member.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import java.time.LocalTime;
+
+@Entity
+@Getter
+public class StudyRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "time", nullable = false)
+    private LocalTime time;
+
+    @JoinColumn(name = "member_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    protected StudyRecord() {}
+
+    public StudyRecord(Long id, LocalTime time, Member member) {
+        this.id = id;
+        this.time = time;
+        this.member = member;
+    }
+}
