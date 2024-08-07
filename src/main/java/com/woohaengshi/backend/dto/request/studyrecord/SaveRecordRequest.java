90b2972ebc4f72f652ef17420ee97182298760fd
rlfrkdms1 <rlfrkdms1@naver.com>
2024-08-08 08:08:49 +0900

    [feat/#4]: request dto 검증 추가

--- src/main/java/com/woohaengshi/backend/dto/request/studyrecord/SaveRecordRequest.java
+++ src/main/java/com/woohaengshi/backend/dto/request/studyrecord/SaveRecordRequest.java
@@ -1,16 +1,21 @@
 package com.woohaengshi.backend.dto.request.studyrecord;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.Getter;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 public class SaveRecordRequest {
 
+    @PastOrPresent
     private LocalDate date;
+    @Min(value = 1, message = "공부 기록은 1초 부터 저장할 수 있습니다. ")
     private int time;
-    private List<String> subjects;
+    private List<String> subjects = new ArrayList<>();
 
     private SaveRecordRequest() {}
 }
