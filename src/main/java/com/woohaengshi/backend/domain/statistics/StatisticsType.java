4cfc765553dbd2a7d09be0a6b1e6e9eaf00dcce2
Kang Hyun Woo <gusdn741021@gmail.com>
2024-08-09 10:46:25 +0900

    [refactor/#8]: StatisticsType리팩토링
     - 필요없는 메소드 제거

--- src/main/java/com/woohaengshi/backend/domain/statistics/StatisticsType.java
+++ src/main/java/com/woohaengshi/backend/domain/statistics/StatisticsType.java
@@ -7,17 +7,4 @@ public enum StatisticsType {
     MONTHLY,
     WEEKLY,
     DAILY;
-
-    public static StatisticsType fromString(String type) {
-        switch (type.toUpperCase()) {
-            case "MONTHLY":
-                return MONTHLY;
-            case "WEEKLY":
-                return WEEKLY;
-            case "DAILY":
-                return DAILY;
-            default:
-                throw new WoohaengshiException(ErrorCode.STATISTICS_TYPE_NOT_FOUND);
-        }
-    }
 }
