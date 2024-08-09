3ad6532fa73ec779cbc985af2ac5e72b01dc0e2e
qbobl5 <ddddd991125@gmail.com>
2024-08-09 10:47:41 +0900

    [feat/#14]: 타이머 조회 기능 추가

---
+++ src/main/java/com/woohaengshi/backend/controller/SubjectController.java
@@ -0,0 +1,21 @@
+package com.woohaengshi.backend.controller;
+
+import com.woohaengshi.backend.dto.response.studyrecord.FindTimerResponse;
+import com.woohaengshi.backend.service.subject.SubjectService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/subject")
+public class SubjectController {
+
+    private final SubjectService subjectService;
+
+    @GetMapping
+    public FindTimerResponse getTimer() {
+        return subjectService.findTimer(1L);
+    }
+}
