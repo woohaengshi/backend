b0b38352ac230a261b671f891d6ebb6fc0c818c7
yaejinkong <0910a@naver.com>
2024-08-09 15:22:03 +0900

    [feat/#7]: SubjectController 추가

--- src/main/java/com/woohaengshi/backend/controller/SubjectController.java
+++ src/main/java/com/woohaengshi/backend/controller/SubjectController.java
@@ -1,2 +1,26 @@
-package com.woohaengshi.backend.controller;public class SubjectController {
+package com.woohaengshi.backend.controller;
+
+import com.woohaengshi.backend.dto.request.SubjectRequestDTO;
+import com.woohaengshi.backend.service.SubjectService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequiredArgsConstructor
+public class SubjectController {
+
+  @Autowired
+  private SubjectService subjectService;
+  @PostMapping("/api/v1/subjects")
+  public ResponseEntity<Void> edit(@RequestBody SubjectRequestDTO requestDTO) {
+    Long memberId = 1L;
+    subjectService.editSubjects(memberId, requestDTO);
+    return ResponseEntity.created(URI.create("/api/v1/subjects")).build();
+  }
 }
