6646a1f9351154333c4d6aa952b16a5ce2ca35f6
qbobl5 <ddddd991125@gmail.com>
2024-08-09 10:01:17 +0900

    [fix/#14]: 타이머 response 로 변경 - 회원 공부 시간 response 에 과목 목록 추가

---
+++ src/main/java/com/woohaengshi/backend/dto/response/studyrecord/FindTimerResponse.java
@@ -0,0 +1,19 @@
+package com.woohaengshi.backend.dto.response.studyrecord;
+
+import com.woohaengshi.backend.dto.response.subject.FindSubjectsResponse;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class FindTimerResponse {
+
+    private int time;
+
+    private List<FindSubjectsResponse> subjectsList;
+
+    public FindTimerResponse(int time, List<FindSubjectsResponse> subjectsList) {
+        this.time = time;
+        this.subjectsList = subjectsList;
+    }
+}
