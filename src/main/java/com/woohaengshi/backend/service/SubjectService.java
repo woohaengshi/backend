8193f27c2a84b3651a70e8b970e6934cac9d7a79
yaejinkong <0910a@naver.com>
2024-08-09 13:29:40 +0900

    [feat/#7]: 과목 추가 기능에서 회원 검증 로직 구현

--- src/main/java/com/woohaengshi/backend/service/SubjectService.java
+++ src/main/java/com/woohaengshi/backend/service/SubjectService.java
@@ -1,2 +1,28 @@
-package com.woohaengshi.backend.service;public class SubjectService {
+package com.woohaengshi.backend.service;
+
+import com.woohaengshi.backend.domain.Subject;
+import com.woohaengshi.backend.domain.member.Member;
+import com.woohaengshi.backend.dto.request.SubjectRequestDTO;
+import com.woohaengshi.backend.exception.ErrorCode;
+import com.woohaengshi.backend.exception.WoohaengshiException;
+import com.woohaengshi.backend.repository.MemberRepository;
+import com.woohaengshi.backend.repository.SubjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Transactional
+@Service
+public class SubjectService {
+
+  @Autowired private SubjectRepository subjectRepository;
+  @Autowired private MemberRepository memberRepository;
+
+  public void saveSubjects(Long member_id, SubjectRequestDTO requestDTO) {
+    List<String> addSubjects = requestDTO.getAddSubjects();
+    Member member = memberRepository.findById(member_id)
+            .orElseThrow(() -> new WoohaengshiException(ErrorCode.MEMBER_NOT_FOUND));
+  }
 }
