340d567e36a45825f8bfbd23d08e212269ba8c11
qbobl5 <ddddd991125@gmail.com>
2024-08-09 12:21:52 +0900

    [test/#14]: 타이머를 조회하는 테스트 추가

---
+++ src/test/java/com/woohaengshi/backend/service/SubjectServiceTest.java
@@ -0,0 +1,72 @@
+package com.woohaengshi.backend.service;
+
+import com.woohaengshi.backend.domain.StudyRecord;
+import com.woohaengshi.backend.domain.Subject;
+import com.woohaengshi.backend.domain.member.Member;
+import com.woohaengshi.backend.dto.response.studyrecord.FindTimerResponse;
+import com.woohaengshi.backend.dto.response.subject.FindSubjectsResponse;
+import com.woohaengshi.backend.repository.MemberRepository;
+import com.woohaengshi.backend.repository.StudyRecordRepository;
+import com.woohaengshi.backend.repository.SubjectRepository;
+import com.woohaengshi.backend.service.subject.SubjectServiceImpl;
+import com.woohaengshi.backend.support.fixture.MemberFixture;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+public class SubjectServiceTest {
+    @Mock private MemberRepository memberRepository;
+    @Mock private StudyRecordRepository studyRecordRepository;
+    @Mock private SubjectRepository subjectRepository;
+
+    @Test
+    void 타이머를_조회_한다() {
+        Member member = MemberFixture.builder().build();
+        LocalDate todayDate = LocalDate.of(2024, 8, 9);
+        int todayStudyTime = 30;
+        Subject subject = Subject.builder().id(1L).name("HTML").member(member).build();
+        FindSubjectsResponse subjectResponse =
+                new FindSubjectsResponse(subject.getId(), subject.getName());
+
+        given(memberRepository.existsById(member.getId())).willReturn(true);
+        given(subjectRepository.findAllByMemberIdOrderByNameAsc(member.getId()))
+                .willReturn(Stream.of(subject));
+        given(studyRecordRepository.findByDateAndMemberId(todayDate, member.getId()))
+                .willReturn(Optional.of(new StudyRecord(1L, todayStudyTime, todayDate, null)));
+
+        SubjectServiceImpl subjectService =
+                new SubjectServiceImpl(studyRecordRepository, subjectRepository, memberRepository);
+
+        FindTimerResponse response = subjectService.findTimer(member.getId());
+
+        List<FindSubjectsResponse> expectedSubjects = Collections.singletonList(subjectResponse);
+        List<FindSubjectsResponse> actualSubjects = response.getSubjectsList();
+
+        assertAll(
+                "response",
+                () -> assertEquals(todayStudyTime, response.getTime(), "Study time should match"),
+                () ->
+                        assertTrue(
+                                expectedSubjects.size() == actualSubjects.size()
+                                        && expectedSubjects
+                                                .get(0)
+                                                .getId()
+                                                .equals(actualSubjects.get(0).getId())
+                                        && expectedSubjects
+                                                .get(0)
+                                                .getName()
+                                                .equals(actualSubjects.get(0).getName()),
+                                "Subjects list should match"));
+    }
+}
