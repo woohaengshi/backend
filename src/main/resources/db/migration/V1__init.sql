20bb74d1b89eb86f0f52dbbfa2b9db82b451d752
rlfrkdms1 <rlfrkdms1@naver.com>
2024-08-09 10:21:23 +0900

    [db/#12]: init.sql 추가

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,72 @@
+CREATE TABLE IF NOT EXISTS `member`
+(
+    `id`         BIGINT       NOT NULL auto_increment,
+    `name`       VARCHAR(255) NOT NULL,
+    `email`      VARCHAR(255) NOT NULL,
+    `password`   VARCHAR(255) NOT NULL,
+    `image`      VARCHAR(255),
+    `course`     VARCHAR(255) NOT NULL,
+    `state`      VARCHAR(255) NOT NULL,
+    `sleep_date` DATE,
+    `created_at` TIMESTAMP    NOT NULL,
+    `updated_at` TIMESTAMP,
+    PRIMARY KEY (`id`)
+)
+    engine = innodb
+    auto_increment = 1
+    DEFAULT charset = utf8mb4
+    COLLATE = utf8mb4_0900_ai_ci;
+
+CREATE TABLE IF NOT EXISTS `badge`
+(
+    `id`         BIGINT       NOT NULL auto_increment,
+    `name`       VARCHAR(255) NOT NULL,
+    `member_id`  BIGINT       NOT NULL,
+    `created_at` TIMESTAMP    NOT NULL,
+    PRIMARY KEY (`id`)
+)
+    engine = innodb
+    auto_increment = 1
+    DEFAULT charset = utf8mb4
+    COLLATE = utf8mb4_0900_ai_ci;
+
+CREATE TABLE IF NOT EXISTS `subject`
+(
+    `id`              BIGINT       NOT NULL auto_increment,
+    `name`            VARCHAR(255) NOT NULL,
+    `member_id`       BIGINT       NOT NULL,
+    `study_record_id` BIGINT       NOT NULL,
+    PRIMARY KEY (`id`)
+)
+    engine = innodb
+    auto_increment = 1
+    DEFAULT charset = utf8mb4
+    COLLATE = utf8mb4_0900_ai_ci;
+
+CREATE TABLE IF NOT EXISTS `study_record`
+(
+    `id`        BIGINT    NOT NULL auto_increment,
+    `time`      MEDIUMINT NOT NULL,
+    `date`      DATE      NOT NULL,
+    `member_id` BIGINT    NOT NULL,
+    PRIMARY KEY (`id`)
+)
+    engine = innodb
+    auto_increment = 1
+    DEFAULT charset = utf8mb4
+    COLLATE = utf8mb4_0900_ai_ci;
+
+CREATE TABLE IF NOT EXISTS `statistics`
+(
+    `id`           BIGINT NOT NULL auto_increment,
+    `monthly_time` MEDIUMINT,
+    `weekly_time`  MEDIUMINT,
+    `daily_time`   MEDIUMINT,
+    `member_id`    BIGINT NOT NULL,
+    PRIMARY KEY (`id`)
+)
+    engine = innodb
+    auto_increment = 1
+    DEFAULT charset = utf8mb4
+    COLLATE = utf8mb4_0900_ai_ci;
+
