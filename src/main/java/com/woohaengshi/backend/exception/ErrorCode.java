27d63d2c2b6fbe5e2988c7b35e892eaf1f4cfefe
qbobl5 <ddddd991125@gmail.com>
2024-08-09 09:21:37 +0900

    [feat/#14]: 공부 기록이 존재하지 않는 경우 예외 처리 설명: 오늘의 공부 누적 시간을 조회하는 경우, 만약 공부 기록이 저장되어 있지 않은 상태라면 필요한 404 error code 추가

--- src/main/java/com/woohaengshi/backend/exception/ErrorCode.java
+++ src/main/java/com/woohaengshi/backend/exception/ErrorCode.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 
 public enum ErrorCode {
     MEMBER_NOT_FOUND(NOT_FOUND, "회원을 찾을 수 없습니다."),
-    INVALID_INPUT(BAD_REQUEST, "잘못된 입력 값입니다.");
+    INVALID_INPUT(BAD_REQUEST, "잘못된 입력 값입니다."),
+    STUDY_RECORD_NOT_FOUND(NOT_FOUND, "오늘의 공부 기록을 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String message;
