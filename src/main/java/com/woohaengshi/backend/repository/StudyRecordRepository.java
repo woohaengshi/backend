9299f5a06904dd5ae32832a340069f66781ab122
qbobl5 <ddddd991125@gmail.com>
2024-08-10 16:45:16 +0900

    [feat/#23]: 연도와 월의 공부 기록 목록 조회

--- src/main/java/com/woohaengshi/backend/repository/StudyRecordRepository.java
+++ src/main/java/com/woohaengshi/backend/repository/StudyRecordRepository.java
@@ -3,10 +3,29 @@ package com.woohaengshi.backend.repository;
 import com.woohaengshi.backend.domain.StudyRecord;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface StudyRecordRepository extends JpaRepository<StudyRecord, Long> {
     Optional<StudyRecord> findByDateAndMemberId(LocalDate date, Long memberId);
+
+    @Query(
+            value =
+                    "SELECT DAY(r.date), r.time, sb.id, sb.subject_name\n"
+                            + "FROM studyrecord r\n"
+                            + "INNER JOIN studysubject st ON r.study_id = st.study_id\n"
+                            + "INNER JOIN subject sb ON st.subject_id = sb.id\n"
+                            + "WHERE YEAR(r.date) = :year\n"
+                            + "  AND MONTH(r.date) = :month\n"
+                            + "  AND r.member_id = :memberId\n"
+                            + "ORDER BY r.date ASC;",
+            nativeQuery = true)
+    List<Object[]> findByYearAndMonthAndMemberId(
+            @Param(value = "year") int year,
+            @Param(value = "month") int month,
+            @Param(value = "memberId") Long memberId);
 }
