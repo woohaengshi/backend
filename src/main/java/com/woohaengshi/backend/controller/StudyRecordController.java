456d9c59c4d2d1d156bda3fb7da3f29b119328ca
qbobl5 <ddddd991125@gmail.com>
2024-08-10 10:32:02 +0900

    [refactor/#22]: study record service 사용하는 부분 리팩토링

--- src/main/java/com/woohaengshi/backend/controller/StudyRecordController.java
+++ src/main/java/com/woohaengshi/backend/controller/StudyRecordController.java
@@ -1,7 +1,7 @@
 package com.woohaengshi.backend.controller;
 
 import com.woohaengshi.backend.dto.request.studyrecord.SaveRecordRequest;
-import com.woohaengshi.backend.service.StudyRecordService;
+import com.woohaengshi.backend.service.studyrecord.StudyRecordServiceImpl;
 
 import jakarta.validation.Valid;
 
@@ -20,11 +20,11 @@ import java.net.URI;
 @RequestMapping("/api/v1/study-record")
 public class StudyRecordController {
 
-    private final StudyRecordService studyRecordService;
+    private final StudyRecordServiceImpl studyRecordServiceImpl;
 
     @PostMapping
     public ResponseEntity<Void> saveStudyRecord(@Valid @RequestBody SaveRecordRequest request) {
-        studyRecordService.save(request, 1L);
+        studyRecordServiceImpl.save(request, 1L);
         return ResponseEntity.created(URI.create("/api/v1/timers")).build();
     }
 }
