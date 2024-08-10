b17900564cb6e624f3e4ccbfda470efc3d548c99
rlfrkdms1 <rlfrkdms1@naver.com>
2024-08-10 22:09:08 +0900

    [refactor/#21]: 사용하지 않는 메서드 삭제

--- src/main/java/com/woohaengshi/backend/repository/SubjectRepository.java
+++ src/main/java/com/woohaengshi/backend/repository/SubjectRepository.java
@@ -4,7 +4,4 @@ import com.woohaengshi.backend.domain.Subject;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SubjectRepository extends JpaRepository<Subject, Long> {
-
-    boolean existsByNameAndStudyRecordId(String name, Long studyRecordId);
-}
+public interface SubjectRepository extends JpaRepository<Subject, Long> {}
