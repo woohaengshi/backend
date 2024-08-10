c1a006d46fa520e8aec302f1a4251ca05e4c842f
yaejinkong <0910a@naver.com>
2024-08-10 23:57:41 +0900

    [refactor/#7]: SubjectController에 @Autowired 삭제

--- src/main/java/com/woohaengshi/backend/controller/SubjectController.java
+++ src/main/java/com/woohaengshi/backend/controller/SubjectController.java
@@ -3,22 +3,18 @@ package com.woohaengshi.backend.controller;
 import com.woohaengshi.backend.dto.request.subject.SubjectRequest;
 import com.woohaengshi.backend.service.SubjectService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URI;
-
 @RestController
 @RequestMapping("/api/v1/subjects")
 @RequiredArgsConstructor
 public class SubjectController {
 
-  @Autowired
-  private SubjectService subjectService;
+  private final SubjectService subjectService;
   @PostMapping
   public ResponseEntity<Void> edit(@RequestBody SubjectRequest requestDTO) {
     Long memberId = 1L;
