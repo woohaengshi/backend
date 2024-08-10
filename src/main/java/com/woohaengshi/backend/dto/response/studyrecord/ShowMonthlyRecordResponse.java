f53fd24505170b768ca51c63b7dd6877d8aa0fcd
qbobl5 <ddddd991125@gmail.com>
2024-08-10 16:27:37 +0900

    [feat/#23]: 특정 월 공부기록 response 추가 - year (연 ex.2024) - month (월 ex.8) - daily (일 별 공부기록 response 목록)

---
+++ src/main/java/com/woohaengshi/backend/dto/response/studyrecord/ShowMonthlyRecordResponse.java
@@ -0,0 +1,38 @@
+package com.woohaengshi.backend.dto.response.studyrecord;
+
+import com.woohaengshi.backend.domain.Subject;
+import lombok.Getter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+public class ShowMonthlyRecordResponse {
+    private int year;
+    private int month;
+    private List<ShowDailyRecordResponse> daily;
+
+    private ShowMonthlyRecordResponse(int year, int month, List<ShowDailyRecordResponse> daily) {
+        this.year = year;
+        this.month = month;
+        this.daily = daily;
+    }
+
+    public static ShowMonthlyRecordResponse of(int year, int month, List<Object[]> records) {
+        return new ShowMonthlyRecordResponse(
+                year,
+                month,
+                records.stream()
+                        .map(
+                                record ->
+                                        ShowDailyRecordResponse.of(
+                                                ((Number) record[0]).intValue(),
+                                                ((Number) record[1]).intValue(),
+                                                List.of(
+                                                        Subject.builder()
+                                                                .id((Long) record[2])
+                                                                .name((String) record[3])
+                                                                .build())))
+                        .collect(Collectors.toList()));
+    }
+}
