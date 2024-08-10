444591d605e81250c7b087cc699646538007ef9c
yaejinkong <0910a@naver.com>
2024-08-10 23:29:01 +0900

    [refactor/#7]: Getter 사용

--- src/main/java/com/woohaengshi/backend/dto/request/subject/SubjectRequest.java
+++ src/main/java/com/woohaengshi/backend/dto/request/subject/SubjectRequest.java
@@ -2,23 +2,17 @@ package com.woohaengshi.backend.dto.request.subject;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
 public class SubjectRequest {
     private List<String> addSubjects;
     private List<Long> deleteSubjects;
-
-    public List<String> getAddSubjects() {
-        return addSubjects;
-    }
-
-    public List<Long> getDeleteSubjects () {
-        return deleteSubjects;
-    }
 
     public void setSubjectsForAddition(List<String> subjects) {
         this.addSubjects = subjects;
