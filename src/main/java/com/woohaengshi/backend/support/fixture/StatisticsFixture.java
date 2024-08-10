4c93a22e4633607e0b5a9f53af720819fab95bbb
Kang Hyun Woo <gusdn741021@gmail.com>
2024-08-10 15:40:52 +0900

    [feat/#8]: StatisticsFixture 추가

---
+++ src/main/java/com/woohaengshi/backend/support/fixture/StatisticsFixture.java
@@ -0,0 +1,54 @@
+package com.woohaengshi.backend.support.fixture;
+
+import com.woohaengshi.backend.domain.member.Member;
+
+public class StatisticsFixture {
+
+    private Long id;
+    private int monthlyTime;
+    private int weeklyTime;
+    private int dailyTime;
+    private int totalTime;
+    private Member member;
+
+    public static StatisticsFixture builder() {
+        return new StatisticsFixture();
+    }
+
+    public StatisticsFixture id(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public StatisticsFixture dailyTime(int time) {
+        this.dailyTime = time;
+        return this;
+    }
+
+    public StatisticsFixture weeklyTime(int time) {
+        this.weeklyTime = time;
+        return this;
+    }
+
+    public StatisticsFixture monthlyTime(int time) {
+        this.monthlyTime = time;
+        return this;
+    }
+    public StatisticsFixture totalTime(int time) {
+        this.totalTime = time;
+        return this;
+    }
+    public StatisticsFixture member(Member member) {
+        this.member = member;
+        return this;
+    }
+
+    public StatisticsFixture build() {
+        return StatisticsFixture.builder().id(id)
+                .dailyTime(dailyTime)
+                .weeklyTime(weeklyTime)
+                .monthlyTime(monthlyTime)
+                .totalTime(totalTime)
+                .member(member).build();
+    }
+}
