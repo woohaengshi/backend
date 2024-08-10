51267cd5eea9575cf78a4bdf9c740742be6e43a0
qbobl5 <ddddd991125@gmail.com>
2024-08-10 16:47:00 +0900

    [feat/#23]: service interface 에 해당하는 월 공부 목록을 조회하는 기능 추가

--- src/main/java/com/woohaengshi/backend/service/studyrecord/StudyRecordService.java
+++ src/main/java/com/woohaengshi/backend/service/studyrecord/StudyRecordService.java
@@ -1,7 +1,10 @@
 package com.woohaengshi.backend.service.studyrecord;
 
 import com.woohaengshi.backend.dto.request.studyrecord.SaveRecordRequest;
+import com.woohaengshi.backend.dto.response.studyrecord.ShowMonthlyRecordResponse;
 
 public interface StudyRecordService {
     public void save(SaveRecordRequest request, Long memberId);
+
+    public ShowMonthlyRecordResponse showMonthlyRecord(int year, int month, Long memberId);
 }
