a5aa8ac10eff04182c3d7ae53b9cc3e1674691be
yaejinkong <0910a@naver.com>
2024-08-10 16:12:57 +0900

    [feat/#7]: 과목 편집 controller 테스트 추가

---
+++ src/test/java/com/woohaengshi/backend/controller/SubjectControllerTest.java
@@ -0,0 +1,40 @@
+package com.woohaengshi.backend.controller;
+
+import com.woohaengshi.backend.dto.request.subject.SubjectRequest;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import java.util.List;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SubjectControllerTest {
+    @LocalServerPort private int port;
+
+    @BeforeEach
+    void setPort() {
+        RestAssured.port = port;
+    }
+
+    @Test
+    void 과목을_편집할_수_있다() {
+        SubjectRequest request = new SubjectRequest();
+        request.setSubjectsForAddition(List.of("Java", "Spring"));
+        request.setSubjectsForDeletion(List.of(1L));
+
+        RestAssured.given()
+                .log()
+                .all()
+                .contentType(ContentType.JSON)
+                .body(request)
+                .when()
+                .post("/api/v1/subjects")
+                .then()
+                .log()
+                .all()
+                .statusCode(201);
+    }
+}
