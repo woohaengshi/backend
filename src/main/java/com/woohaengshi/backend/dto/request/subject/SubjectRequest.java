f34ff76823a18649cda44acb33f44fbd90aee60e
yaejinkong <0910a@naver.com>
2024-08-10 23:42:35 +0900

    [refactor/#7]: @NoArgsConstructor와 @AllArgsConstructor 제거후 생성자 추가

--- src/main/java/com/woohaengshi/backend/dto/request/subject/SubjectRequest.java
+++ src/main/java/com/woohaengshi/backend/dto/request/subject/SubjectRequest.java
@@ -1,18 +1,18 @@
 package com.woohaengshi.backend.dto.request.subject;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-@NoArgsConstructor
-@AllArgsConstructor
 @Getter
 public class SubjectRequest {
     private List<String> addSubjects;
     private List<Long> deleteSubjects;
+
+    public SubjectRequest(List<String> addSubjects, List<Long> deleteSubjects) {
+        this.addSubjects = addSubjects;
+        this.deleteSubjects = deleteSubjects;
+    }
 
     public void setSubjectsForAddition(List<String> subjects) {
         this.addSubjects = subjects;
