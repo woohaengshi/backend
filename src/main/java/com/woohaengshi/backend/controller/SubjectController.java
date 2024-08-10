d535f8d5e9beadc15a9fe2b51a573c4d014228cc
yaejinkong <0910a@naver.com>
2024-08-10 23:15:14 +0900

    [refactor/#7]: SubjectController 클래스에 공통 경로 매핑 적용

--- src/main/java/com/woohaengshi/backend/controller/SubjectController.java
+++ src/main/java/com/woohaengshi/backend/controller/SubjectController.java
@@ -7,17 +7,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 
 @RestController
+@RequestMapping("/api/v1/subjects")
 @RequiredArgsConstructor
 public class SubjectController {
 
   @Autowired
   private SubjectService subjectService;
-  @PostMapping("/api/v1/subjects")
+  @PostMapping
   public ResponseEntity<Void> edit(@RequestBody SubjectRequest requestDTO) {
     Long memberId = 1L;
     subjectService.editSubjects(memberId, requestDTO);
