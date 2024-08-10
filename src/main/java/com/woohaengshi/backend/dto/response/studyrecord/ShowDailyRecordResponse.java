de17f0b1fa4cd7563c46051643c317f86455f349
qbobl5 <ddddd991125@gmail.com>
2024-08-10 16:26:05 +0900

    [feat/#23]: 일 별 공부기록 response 추가 - day (일 ex. 2) - time (공부한 시간(초) ex. 36000) - subjects (공부한 과목 목록)

---
+++ src/main/java/com/woohaengshi/backend/dto/response/studyrecord/ShowDailyRecordResponse.java
@@ -0,0 +1,25 @@
+package com.woohaengshi.backend.dto.response.studyrecord;
+
+import com.woohaengshi.backend.domain.Subject;
+import com.woohaengshi.backend.dto.response.subject.ShowSubjectsResponse;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class ShowDailyRecordResponse {
+    private int day;
+    private int time;
+    private List<ShowSubjectsResponse> subjects;
+
+    private ShowDailyRecordResponse(int day, int time, List<ShowSubjectsResponse> subjects) {
+        this.day = day;
+        this.time = time;
+        this.subjects = subjects;
+    }
+
+    public static ShowDailyRecordResponse of(int day, int time, List<Subject> subjects) {
+        return new ShowDailyRecordResponse(
+                day, time, subjects.stream().map(ShowSubjectsResponse::from).toList());
+    }
+}
