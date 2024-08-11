85dd0e249b966743d553d14ae62af2a93c21f77d
rlfrkdms1 <rlfrkdms1@naver.com>
2024-08-11 14:05:21 +0900

    [feat/#18]: memberId argument resolver 추가
    - 검증 기능
    - memberId 추출 기능
    - 만료 기간 조회 기능

---
+++ src/main/java/com/woohaengshi/backend/controller/auth/AuthArgumentResolver.java
@@ -0,0 +1,60 @@
+package com.woohaengshi.backend.controller.auth;
+
+import com.woohaengshi.backend.exception.WoohaengshiException;
+import com.woohaengshi.backend.service.auth.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import static com.woohaengshi.backend.exception.ErrorCode.INCORRECT_CONSTRUCT_HEADER;
+import static com.woohaengshi.backend.exception.ErrorCode.NOT_EXIST_ACCESS_TOKEN;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+@Component
+@RequiredArgsConstructor
+public class AuthArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private static final String TOKEN_TYPE = "Bearer";
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(MemberId.class)
+                && Long.class.isAssignableFrom(parameter.getParameterType());
+    }
+
+    @Override
+    public Object resolveArgument(
+            MethodParameter parameter,
+            ModelAndViewContainer mavContainer,
+            NativeWebRequest webRequest,
+            WebDataBinderFactory binderFactory)
+            throws Exception {
+        String authorization = webRequest.getHeader(AUTHORIZATION);
+        MemberId memberId = parameter.getParameterAnnotation(MemberId.class);
+        if (!memberId.required()) {
+            return null;
+        }
+        validateExistAuthHeader(authorization);
+        String accessToken = getAccessToken(authorization);
+        return jwtTokenProvider.getMemberId(accessToken);
+    }
+
+    private void validateExistAuthHeader(String authorization) {
+        if (authorization == null) {
+            throw new WoohaengshiException(NOT_EXIST_ACCESS_TOKEN);
+        }
+    }
+
+    private String getAccessToken(String authorization) {
+        String[] tokenFormat = authorization.split(" ");
+        if (tokenFormat.length != 2 && !tokenFormat[0].equals(TOKEN_TYPE)) {
+            throw new WoohaengshiException(INCORRECT_CONSTRUCT_HEADER);
+        }
+        return tokenFormat[1];
+    }
+}
--- src/main/java/com/woohaengshi/backend/exception/ErrorCode.java
+++ src/main/java/com/woohaengshi/backend/exception/ErrorCode.java
@@ -15,7 +15,9 @@ public enum ErrorCode {
     UNSUPPORTED_TOKEN(UNAUTHORIZED, "지원하지 않는 토큰 형식입니다"),
     INCORRECTLY_CONSTRUCTED_TOKEN(UNAUTHORIZED, "잘못된 구조의 토큰입니다"),
     FAILED_SIGNATURE_TOKEN(UNAUTHORIZED, "서명에 실패한 토큰입니다"),
-    INVALID_CLAIM_TYPE(HttpStatus.UNAUTHORIZED, "토큰의 claim값은 Long 타입이어야 합니다");
+    INVALID_CLAIM_TYPE(UNAUTHORIZED, "토큰의 claim값은 Long 타입이어야 합니다"),
+    NOT_EXIST_ACCESS_TOKEN(UNAUTHORIZED, "액세스 토큰이 존재하지 않습니다."),
+    INCORRECT_CONSTRUCT_HEADER(UNAUTHORIZED, "잘못된 형식의 인증 헤더입니다.");
 
 
 
