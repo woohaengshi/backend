8c7564162534286eb10c7b3c47bc1eeb62ccfab7
rlfrkdms1 <rlfrkdms1@naver.com>
2024-08-11 10:42:11 +0900

    [feat/#18]: jwt 토큰과 관련된 기능 추가
    - 검증 기능
    - memberId 추출 기능
    - 만료 기간 조회 기능

--- src/main/java/com/woohaengshi/backend/exception/ErrorCode.java
+++ src/main/java/com/woohaengshi/backend/exception/ErrorCode.java
@@ -1,13 +1,23 @@
 package com.woohaengshi.backend.exception;
 
+import org.springframework.http.HttpStatus;
+
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
-
-import org.springframework.http.HttpStatus;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 public enum ErrorCode {
     MEMBER_NOT_FOUND(NOT_FOUND, "회원을 찾을 수 없습니다."),
-    INVALID_INPUT(BAD_REQUEST, "잘못된 입력 값입니다.");
+    INVALID_INPUT(BAD_REQUEST, "잘못된 입력 값입니다."),
+    MISSING_ISSUER_TOKEN(UNAUTHORIZED, "issuer가 존재하지 않는 토큰입니다"),
+    NOT_WOOHAENGSHI_TOKEN(UNAUTHORIZED, "발급자가 잘못된 토큰입니다"),
+    EXPIRED_TOKEN(UNAUTHORIZED, "토큰이 만료되었습니다"),
+    UNSUPPORTED_TOKEN(UNAUTHORIZED, "지원하지 않는 토큰 형식입니다"),
+    INCORRECTLY_CONSTRUCTED_TOKEN(UNAUTHORIZED, "잘못된 구조의 토큰입니다"),
+    FAILED_SIGNATURE_TOKEN(UNAUTHORIZED, "서명에 실패한 토큰입니다"),
+    INVALID_CLAIM_TYPE(HttpStatus.UNAUTHORIZED, "토큰의 claim값은 Long 타입이어야 합니다");
+
+
 
     private final HttpStatus status;
     private final String message;
--- src/main/java/com/woohaengshi/backend/service/auth/JwtTokenProvider.java
+++ src/main/java/com/woohaengshi/backend/service/auth/JwtTokenProvider.java
@@ -1,13 +1,25 @@
 package com.woohaengshi.backend.service.auth;
 
+import com.woohaengshi.backend.exception.WoohaengshiException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.IncorrectClaimException;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.MissingClaimException;
+import io.jsonwebtoken.RequiredTypeException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
+
+import static com.woohaengshi.backend.exception.ErrorCode.*;
 
 @Component
 public class JwtTokenProvider {
@@ -43,5 +55,39 @@ public class JwtTokenProvider {
 
     public String createRefreshToken(Long memberId) {
         return createToken(memberId, refreshExpiration);
+    }
+
+    public void validToken(String token) {
+        try {
+            getClaimsJwt(token);
+        } catch (MissingClaimException e) {
+            throw new WoohaengshiException(MISSING_ISSUER_TOKEN);
+        } catch (IncorrectClaimException e) {
+            throw new WoohaengshiException(NOT_WOOHAENGSHI_TOKEN);
+        } catch (ExpiredJwtException e) {
+            throw new WoohaengshiException(EXPIRED_TOKEN);
+        } catch (UnsupportedJwtException e) {
+            throw new WoohaengshiException(UNSUPPORTED_TOKEN);
+        } catch (SignatureException e) {
+            throw new WoohaengshiException(FAILED_SIGNATURE_TOKEN);
+        } catch (MalformedJwtException e) {
+            throw new WoohaengshiException(INCORRECTLY_CONSTRUCTED_TOKEN);
+        }
+    }
+
+    public Jws<Claims> getClaimsJwt(String token) {
+        return Jwts.parser().verifyWith(key).requireIssuer(ISSUER).build().parseClaimsJws(token);
+    }
+
+    public Long getMemberId(String token) {
+        try {
+            return getClaimsJwt(token).getPayload().get(MEMBER_ID, Long.class);
+        } catch (RequiredTypeException e) {
+            throw new WoohaengshiException(INVALID_CLAIM_TYPE);
+        }
+    }
+
+    public Date getExpiredAt(String accessToken) {
+        return getClaimsJwt(accessToken).getBody().getExpiration();
     }
 }
