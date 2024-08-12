1aaaacb377269512f46227b357fa92bdd2dc9e6a
qbobl5 <ddddd991125@gmail.com>
2024-08-12 22:26:42 +0900

    [feat/#48]: 연도 별로 공부 기록 시간을 조회하는 메소드 추가

--- src/main/java/com/woohaengshi/backend/service/studyrecord/StudyRecordService.java
+++ src/main/java/com/woohaengshi/backend/service/studyrecord/StudyRecordService.java
@@ -2,6 +2,7 @@ package com.woohaengshi.backend.service.studyrecord;
 
 import com.woohaengshi.backend.dto.request.studyrecord.SaveRecordRequest;
 import com.woohaengshi.backend.dto.response.studyrecord.ShowMonthlyRecordResponse;
+import com.woohaengshi.backend.dto.response.studyrecord.ShowYearlyRecordResponse;
 
 import java.time.YearMonth;
 
@@ -9,4 +10,6 @@ public interface StudyRecordService {
     public void save(SaveRecordRequest request, Long memberId);
 
     public ShowMonthlyRecordResponse showMonthlyRecord(YearMonth date, Long memberId);
+
+    public ShowYearlyRecordResponse showYearlyRecord(int year, Long memberId);
 }
