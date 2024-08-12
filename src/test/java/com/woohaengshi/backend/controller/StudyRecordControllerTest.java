fa0904448b251798909830e78e8e86bf16a961f2
rlfrkdms1 <rlfrkdms1@naver.com>
2024-08-13 08:50:46 +0900

    [test/#18]: 컨트롤러 테스트에 헤더 추가

--- src/test/java/com/woohaengshi/backend/controller/StudyRecordControllerTest.java
+++ src/test/java/com/woohaengshi/backend/controller/StudyRecordControllerTest.java
@@ -2,16 +2,22 @@ package com.woohaengshi.backend.controller;
 
 import static com.woohaengshi.backend.exception.ErrorCode.INVALID_INPUT;
 
+import static org.apache.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.OK;
 
 import com.woohaengshi.backend.dto.request.studyrecord.SaveRecordRequest;
 
+import com.woohaengshi.backend.service.auth.JwtTokenProvider;
+import com.woohaengshi.backend.support.ControllerTest;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
@@ -19,22 +25,15 @@ import java.time.LocalDate;
 import java.time.YearMonth;
 import java.util.List;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class StudyRecordControllerTest {
-    @LocalServerPort private int port;
 
-    @BeforeEach
-    void setPort() {
-        RestAssured.port = port;
-    }
+class StudyRecordControllerTest extends ControllerTest {
+
+    @Autowired private JwtTokenProvider jwtTokenProvider;
 
     @Test
     void 공부를_기록할_수_있다() {
         SaveRecordRequest request = new SaveRecordRequest(LocalDate.now(), 10, List.of(1L, 2L));
-        RestAssured.given()
-                .log()
-                .all()
-                .contentType(ContentType.JSON)
+        baseRestAssured()
                 .body(request)
                 .when()
                 .post("/api/v1/study-record")
@@ -47,10 +46,7 @@ class StudyRecordControllerTest {
     @Test
     void 시간이_0일경우_공부를_기록할_수_없다() {
         SaveRecordRequest request = new SaveRecordRequest(LocalDate.now(), 0, List.of(1L, 2L));
-        RestAssured.given()
-                .log()
-                .all()
-                .contentType(ContentType.JSON)
+        baseRestAssured()
                 .body(request)
                 .when()
                 .post("/api/v1/study-record")
@@ -64,10 +60,7 @@ class StudyRecordControllerTest {
     void 날짜가_미래일_경우_공부를_기록할_수_없다() {
         SaveRecordRequest request =
                 new SaveRecordRequest(LocalDate.now().plusDays(2), 0, List.of(1L, 2L));
-        RestAssured.given()
-                .log()
-                .all()
-                .contentType(ContentType.JSON)
+        baseRestAssured()
                 .body(request)
                 .when()
                 .post("/api/v1/study-record")
@@ -79,10 +72,7 @@ class StudyRecordControllerTest {
 
     @Test
     void 연도와_월을_통해_공부_기록을_조회할_수_있다() {
-        RestAssured.given()
-                .log()
-                .all()
-                .contentType(ContentType.JSON)
+        baseRestAssured()
                 .queryParam("date", YearMonth.now().toString())
                 .when()
                 .get("/api/v1/study-record/monthly")
@@ -94,15 +84,20 @@ class StudyRecordControllerTest {
 
     @Test
     void 날짜가_전달되지_않으면_공부_기록을_조회할_수_없다() {
-        RestAssured.given()
-                .log()
-                .all()
-                .contentType(ContentType.JSON)
+        baseRestAssured()
                 .when()
                 .get("/api/v1/study-record/monthly")
                 .then()
                 .log()
                 .all()
                 .statusCode(BAD_REQUEST.value());
+    }
+
+    private RequestSpecification baseRestAssured() {
+        return RestAssured.given()
+                .header(AUTHORIZATION, "Bearer " + jwtTokenProvider.createAccessToken(1L))
+                .log()
+                .all()
+                .contentType(ContentType.JSON);
     }
 }
