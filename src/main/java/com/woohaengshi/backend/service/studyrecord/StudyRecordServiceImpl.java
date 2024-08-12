e1ece133e4a14f68ef21ed75ae630e5cc477d40a
qbobl5 <ddddd991125@gmail.com>
2024-08-12 10:11:30 +0900

    [fix/#23]: confilct 해결

--- src/main/java/com/woohaengshi/backend/service/studyrecord/StudyRecordServiceImpl.java
+++ src/main/java/com/woohaengshi/backend/service/studyrecord/StudyRecordServiceImpl.java
@@ -1,14 +1,18 @@
 package com.woohaengshi.backend.service.studyrecord;
 
+import static com.woohaengshi.backend.exception.ErrorCode.MEMBER_NOT_FOUND;
+import static com.woohaengshi.backend.exception.ErrorCode.SUBJECT_NOT_FOUND;
+
 import com.woohaengshi.backend.domain.StudyRecord;
+import com.woohaengshi.backend.domain.StudySubject;
 import com.woohaengshi.backend.domain.Subject;
 import com.woohaengshi.backend.domain.member.Member;
 import com.woohaengshi.backend.dto.request.studyrecord.SaveRecordRequest;
 import com.woohaengshi.backend.dto.response.studyrecord.ShowMonthlyRecordResponse;
-import com.woohaengshi.backend.exception.ErrorCode;
 import com.woohaengshi.backend.exception.WoohaengshiException;
 import com.woohaengshi.backend.repository.MemberRepository;
 import com.woohaengshi.backend.repository.StudyRecordRepository;
+import com.woohaengshi.backend.repository.StudySubjectRepository;
 import com.woohaengshi.backend.repository.SubjectRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -20,15 +24,16 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
+@Transactional
 public class StudyRecordServiceImpl implements StudyRecordService {
 
     private final MemberRepository memberRepository;
     private final StudyRecordRepository studyRecordRepository;
     private final SubjectRepository subjectRepository;
+    private final StudySubjectRepository studySubjectRepository;
 
-    @Transactional
+    @Override
     public void save(SaveRecordRequest request, Long memberId) {
         validateExistMember(memberId);
         Optional<StudyRecord> optionalStudyRecord =
@@ -37,6 +42,7 @@ public class StudyRecordServiceImpl implements StudyRecordService {
         saveSubjects(request.getSubjects(), studyRecord);
     }
 
+    @Override
     @Transactional(readOnly = true)
     public ShowMonthlyRecordResponse showMonthlyRecord(int year, int month, Long memberId) {
         validateExistMember(memberId);
@@ -48,7 +54,7 @@ public class StudyRecordServiceImpl implements StudyRecordService {
 
     private void validateExistMember(Long memberId) {
         if (!memberRepository.existsById(memberId))
-            throw new WoohaengshiException(ErrorCode.MEMBER_NOT_FOUND);
+            throw new WoohaengshiException(MEMBER_NOT_FOUND);
     }
 
     private StudyRecord saveStudyRecord(
@@ -61,26 +67,29 @@ public class StudyRecordServiceImpl implements StudyRecordService {
         return studyRecordRepository.save(request.toStudyRecord(findMemberById(memberId)));
     }
 
-    private void saveSubjects(List<String> subjects, StudyRecord studyRecord) {
-        subjects.forEach(
-                subject -> {
-                    if (!subjectRepository.existsByNameAndStudyRecordId(
-                            subject, studyRecord.getId()))
-                        subjectRepository.save(createSubject(studyRecord, subject));
-                });
+    private void saveSubjects(List<Long> subjects, StudyRecord studyRecord) {
+        for (Long subjectId : subjects) {
+            if (!studySubjectRepository.existsBySubjectIdAndStudyRecordId(
+                    subjectId, studyRecord.getId())) {
+                Subject subject = findSubjectById(subjectId);
+                studySubjectRepository.save(createStudySubject(studyRecord, subject));
+            }
+        }
     }
 
-    private Subject createSubject(StudyRecord studyRecord, String subject) {
-        return Subject.builder()
-                .name(subject)
-                .studyRecord(studyRecord)
-                .member(studyRecord.getMember())
-                .build();
+    private Subject findSubjectById(Long subjectId) {
+        return subjectRepository
+                .findById(subjectId)
+                .orElseThrow(() -> new WoohaengshiException(SUBJECT_NOT_FOUND));
+    }
+
+    private StudySubject createStudySubject(StudyRecord studyRecord, Subject subject) {
+        return StudySubject.builder().subject(subject).studyRecord(studyRecord).build();
     }
 
     private Member findMemberById(Long memberId) {
         return memberRepository
                 .findById(memberId)
-                .orElseThrow(() -> new WoohaengshiException(ErrorCode.MEMBER_NOT_FOUND));
+                .orElseThrow(() -> new WoohaengshiException(MEMBER_NOT_FOUND));
     }
 }
