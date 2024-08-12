86493cfc0c1b9462f7f5f10f0b1a837c7106c9b7
qbobl5 <ddddd991125@gmail.com>
2024-08-12 22:25:53 +0900

    [feat/#48]: 연도에 해당하는 전체 월(공부 기록이 있는) 및 해당 월의 총 공부 시간을 저장하는 response dto - year (연도) - monthly (월 별 기록)

---
+++ src/main/java/com/woohaengshi/backend/dto/response/studyrecord/ShowYearlyRecordResponse.java
@@ -0,0 +1,21 @@
+package com.woohaengshi.backend.dto.response.studyrecord;
+
+import com.woohaengshi.backend.dto.result.MonthlyTotalRecordResult;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class ShowYearlyRecordResponse {
+    private int year;
+    private List<MonthlyTotalRecordResult> monthly;
+
+    private ShowYearlyRecordResponse(int year, List<MonthlyTotalRecordResult> monthly) {
+        this.year = year;
+        this.monthly = monthly;
+    }
+
+    public static ShowYearlyRecordResponse of(int year, List<MonthlyTotalRecordResult> monthly) {
+        return new ShowYearlyRecordResponse(year, monthly);
+    }
+}
