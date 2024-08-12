59d811bbefcb08ba82e773284871bafc40bacd12
rlfrkdms1 <rlfrkdms1@naver.com>
2024-08-13 08:54:09 +0900

    [test/#18]: 필요없는 import문 삭제

--- src/test/java/com/woohaengshi/backend/controller/StudyRecordControllerTest.java
+++ src/test/java/com/woohaengshi/backend/controller/StudyRecordControllerTest.java
@@ -14,12 +14,8 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
 import io.restassured.specification.RequestSpecification;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 
 import java.time.LocalDate;
 import java.time.YearMonth;
