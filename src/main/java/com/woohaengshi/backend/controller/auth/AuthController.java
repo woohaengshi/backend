2f1d08caf1b3775bc4f1ebf3f60d3a3c69ce0fba
rlfrkdms1 <rlfrkdms1@naver.com>
2024-08-12 17:46:35 +0900

    [feat/#18]: 로그인 컨트롤러 추가

--- src/main/java/com/woohaengshi/backend/controller/auth/AuthController.java
+++ src/main/java/com/woohaengshi/backend/controller/auth/AuthController.java
@@ -2,13 +2,17 @@ package com.woohaengshi.backend.controller.auth;
 
 import com.woohaengshi.backend.dto.request.studyrecord.auth.SignInRequest;
 import com.woohaengshi.backend.dto.response.auth.SignInResponse;
+import com.woohaengshi.backend.dto.result.SignInResult;
 import com.woohaengshi.backend.service.auth.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpHeaders.SET_COOKIE;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,7 +22,10 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/sign-in")
-    public SignInResponse signIn(@RequestBody @Valid SignInRequest signInRequest) {
-        return authService.signIn(signInRequest);
+    public ResponseEntity<SignInResponse> signIn(@RequestBody @Valid SignInRequest signInRequest) {
+        SignInResult result = authService.signIn(signInRequest);
+        return ResponseEntity.ok()
+                .header(SET_COOKIE, result.getRefreshTokenCookie().toString())
+                .body(result.getSignInResponse());
     }
 }
