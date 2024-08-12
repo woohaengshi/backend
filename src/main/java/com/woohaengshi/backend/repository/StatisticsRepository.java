cc4660af99186de002c9af897a770e5e360ead79
Kang Hyun Woo <gusdn741021@gmail.com>
2024-08-13 08:52:35 +0900

    [feat/#8]: 모든 통계 인스턴스 불러오는 쿼리 작성
     - 모든 통계 인스턴스와 멤버를 가져오는 쿼리 작성

--- src/main/java/com/woohaengshi/backend/repository/StatisticsRepository.java
+++ src/main/java/com/woohaengshi/backend/repository/StatisticsRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StatisticsRepository
@@ -13,4 +14,7 @@ public interface StatisticsRepository
 
     @Query("SELECT s FROM Statistics s JOIN FETCH s.member WHERE s.member.id = :memberId")
     Optional<Statistics> findByMemberId(Long memberId);
+
+    @Query("SELECT s FROM Statistics s JOIN FETCH s.member")
+    List<Statistics> findAllWithMember();
 }
