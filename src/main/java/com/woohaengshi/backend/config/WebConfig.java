1f72b2a2517a3b7190a516b8f216f99884930978
rlfrkdms1 <rlfrkdms1@naver.com>
2024-08-12 15:22:36 +0900

    [feat/#18]: CORS 설정 추가

---
+++ src/main/java/com/woohaengshi/backend/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.woohaengshi.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig  implements WebMvcConfigurer {
+
+    private static final String CLIENT_LOCALHOST = "http://localhost:3000";
+    private static final String CORS_ALLOWED_METHODS = "GET,POST,HEAD,PUT,PATCH,DELETE,TRACE,OPTIONS";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedMethods(CORS_ALLOWED_METHODS.split(","))
+                .allowedOrigins(CLIENT_LOCALHOST)
+                .exposedHeaders(HttpHeaders.SET_COOKIE, HttpHeaders.LOCATION)
+                .allowCredentials(true);
+    }
+}
