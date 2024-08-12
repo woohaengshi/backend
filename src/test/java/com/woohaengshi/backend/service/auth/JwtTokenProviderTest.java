a5fbced60bb460469c911ffafbde04f3a23a16cd
rlfrkdms1 <rlfrkdms1@naver.com>
2024-08-12 15:18:22 +0900

    [feat/#18]: 유효기간 지난 토큰 예외 테스트

---
+++ src/test/java/com/woohaengshi/backend/service/auth/JwtTokenProviderTest.java
@@ -0,0 +1,22 @@
+package com.woohaengshi.backend.service.auth;
+
+import com.woohaengshi.backend.exception.WoohaengshiException;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class JwtTokenProviderTest {
+
+    private static final String key =
+            "55e32f10a70c4362a393055896bb17c955e32f10a70c4362a393055896bb17c9";
+    private static final Long accessExpiration = 100L;
+    private static final Long refreshExpiration = 1000L;
+
+    @Test
+    void 유효기간이_지난_토큰은_예외를_던진다() {
+        JwtTokenProvider jwtTokenProvider = new JwtTokenProvider(key, 0L, 0L);
+        String accessToken = jwtTokenProvider.createAccessToken(1L);
+        assertThatThrownBy(() -> jwtTokenProvider.validToken(accessToken))
+                .isExactlyInstanceOf(WoohaengshiException.class);
+    }
+}
