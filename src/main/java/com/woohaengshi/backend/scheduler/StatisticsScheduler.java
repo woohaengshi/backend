902fd4fdbbb7427dd9d3e92cdf4fc4ef4ae48d82
Kang Hyun Woo <gusdn741021@gmail.com>
2024-08-13 08:49:51 +0900

    [feat/#8]: 통계 배치할 메소드 정의
     - 오전 5시간 마다 실행할 메소드 정의

--- src/main/java/com/woohaengshi/backend/scheduler/StatisticsScheduler.java
+++ src/main/java/com/woohaengshi/backend/scheduler/StatisticsScheduler.java
@@ -1,3 +1,16 @@
 package com.woohaengshi.backend.scheduler;
 
-public class StatisticsScheduler {}
+import com.woohaengshi.backend.service.statistics.StatisticsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class StatisticsScheduler {
+    private final StatisticsService statisticsService;
+
+    @Scheduled(cron = "0 0 5 * * ?")
+    public void task(){
+    }
+}
