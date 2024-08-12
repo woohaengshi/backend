b709572f5c8a6d7ca9d2f5f457881a437306af43
Kang Hyun Woo <gusdn741021@gmail.com>
2024-08-12 14:33:49 +0900

    [refactor/#8]:  필요없는 import 제거
     - HttpStatus 제거

--- src/main/java/com/woohaengshi/backend/exception/ErrorCode.java
+++ src/main/java/com/woohaengshi/backend/exception/ErrorCode.java
@@ -1,6 +1,5 @@
 package com.woohaengshi.backend.exception;
 
-import static org.springframework.http.HttpStatus.*;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
