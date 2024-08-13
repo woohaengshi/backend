9846962e074554d4b352dac1ba596c4b7b4e2136
rlfrkdms1 <rlfrkdms1@naver.com>
2024-08-13 10:25:49 +0900

    [feat/#18]: 리프레시 토큰 repository 추가

--- src/main/java/com/woohaengshi/backend/repository/RefreshTokenRepository.java
+++ src/main/java/com/woohaengshi/backend/repository/RefreshTokenRepository.java
@@ -2,5 +2,12 @@ package com.woohaengshi.backend.repository;
 
 import com.woohaengshi.backend.domain.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {}
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    @Query("select r from RefreshToken r join fetch r.member where r.token = :token")
+    Optional<RefreshToken> findByToken(@Param("token") String token);
+}
