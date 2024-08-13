3d840e556ac6e31567ff09fcb7c4f21e488bcb5f
rlfrkdms1 <rlfrkdms1@naver.com>
2024-08-13 15:31:07 +0900

    [test/#18]: 회원 가입 컨트롤러 테스트

--- src/test/java/com/woohaengshi/backend/controller/auth/AuthControllerTest.java
+++ src/test/java/com/woohaengshi/backend/controller/auth/AuthControllerTest.java
@@ -1,10 +1,13 @@
 package com.woohaengshi.backend.controller.auth;
 
+import com.woohaengshi.backend.domain.member.Course;
+import com.woohaengshi.backend.dto.request.auth.SignUpRequest;
 import com.woohaengshi.backend.dto.request.studyrecord.auth.SignInRequest;
 import com.woohaengshi.backend.support.ControllerTest;
 import org.junit.jupiter.api.Test;
 
 import static com.woohaengshi.backend.controller.auth.RefreshCookieProvider.REFRESH_TOKEN;
+import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
 
 class AuthControllerTest extends ControllerTest {
@@ -21,5 +24,18 @@ class AuthControllerTest extends ControllerTest {
                 .all()
                 .cookie(REFRESH_TOKEN)
                 .statusCode(OK.value());
+    }
+
+    @Test
+    void 회원가입이_가능하다() {
+        SignUpRequest request = new SignUpRequest("김혜빈", Course.CLOUD_SERVICE, "rlagpqls@naver.com", "password12!@");
+        baseRestAssured()
+                .body(request)
+                .when()
+                .post("/api/v1/sign-up")
+                .then()
+                .log()
+                .all()
+                .statusCode(CREATED.value());
     }
 }
