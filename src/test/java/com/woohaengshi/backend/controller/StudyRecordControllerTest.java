756daf6898e3f31d0fb0fdc10aa53251b614652a
rlfrkdms1 <rlfrkdms1@naver.com>
2024-08-13 11:39:14 +0900

    [test/#18]: controllerTest에 인가관련 설정 추가

--- src/test/java/com/woohaengshi/backend/controller/StudyRecordControllerTest.java
+++ src/test/java/com/woohaengshi/backend/controller/StudyRecordControllerTest.java
@@ -2,20 +2,13 @@ package com.woohaengshi.backend.controller;
 
 import static com.woohaengshi.backend.exception.ErrorCode.INVALID_INPUT;
 
-import static org.apache.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.OK;
 
 import com.woohaengshi.backend.dto.request.studyrecord.SaveRecordRequest;
 
-import com.woohaengshi.backend.service.auth.JwtTokenProvider;
 import com.woohaengshi.backend.support.ControllerTest;
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-
-import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDate;
 import java.time.YearMonth;
@@ -24,12 +17,10 @@ import java.util.List;
 
 class StudyRecordControllerTest extends ControllerTest {
 
-    @Autowired private JwtTokenProvider jwtTokenProvider;
-
     @Test
     void 공부를_기록할_수_있다() {
         SaveRecordRequest request = new SaveRecordRequest(LocalDate.now(), 10, List.of(1L, 2L));
-        baseRestAssured()
+        baseRestAssuredWithAuth()
                 .body(request)
                 .when()
                 .post("/api/v1/study-record")
@@ -42,7 +33,7 @@ class StudyRecordControllerTest extends ControllerTest {
     @Test
     void 시간이_0일경우_공부를_기록할_수_없다() {
         SaveRecordRequest request = new SaveRecordRequest(LocalDate.now(), 0, List.of(1L, 2L));
-        baseRestAssured()
+        baseRestAssuredWithAuth()
                 .body(request)
                 .when()
                 .post("/api/v1/study-record")
@@ -56,7 +47,7 @@ class StudyRecordControllerTest extends ControllerTest {
     void 날짜가_미래일_경우_공부를_기록할_수_없다() {
         SaveRecordRequest request =
                 new SaveRecordRequest(LocalDate.now().plusDays(2), 0, List.of(1L, 2L));
-        baseRestAssured()
+        baseRestAssuredWithAuth()
                 .body(request)
                 .when()
                 .post("/api/v1/study-record")
@@ -68,7 +59,7 @@ class StudyRecordControllerTest extends ControllerTest {
 
     @Test
     void 연도와_월을_통해_공부_기록을_조회할_수_있다() {
-        baseRestAssured()
+        baseRestAssuredWithAuth()
                 .queryParam("date", YearMonth.now().toString())
                 .when()
                 .get("/api/v1/study-record/monthly")
@@ -80,7 +71,7 @@ class StudyRecordControllerTest extends ControllerTest {
 
     @Test
     void 날짜가_전달되지_않으면_공부_기록을_조회할_수_없다() {
-        baseRestAssured()
+        baseRestAssuredWithAuth()
                 .when()
                 .get("/api/v1/study-record/monthly")
                 .then()
@@ -89,11 +80,4 @@ class StudyRecordControllerTest extends ControllerTest {
                 .statusCode(BAD_REQUEST.value());
     }
 
-    private RequestSpecification baseRestAssured() {
-        return RestAssured.given()
-                .header(AUTHORIZATION, "Bearer " + jwtTokenProvider.createAccessToken(1L))
-                .log()
-                .all()
-                .contentType(ContentType.JSON);
-    }
 }
