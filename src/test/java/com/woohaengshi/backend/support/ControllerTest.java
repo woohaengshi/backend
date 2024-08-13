6d2935224b030a39b89b335c3f1b618179bbb6fd
rlfrkdms1 <rlfrkdms1@naver.com>
2024-08-13 11:39:02 +0900

    [test/#18]: controllerTest에 인가관련 설정 추가

--- src/test/java/com/woohaengshi/backend/support/ControllerTest.java
+++ src/test/java/com/woohaengshi/backend/support/ControllerTest.java
@@ -1,9 +1,15 @@
 package com.woohaengshi.backend.support;
 
+import com.woohaengshi.backend.service.auth.JwtTokenProvider;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.apache.http.HttpHeaders.AUTHORIZATION;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ControllerTest {
@@ -11,8 +17,26 @@ public class ControllerTest {
     @LocalServerPort
     private int port;
 
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
     @BeforeEach
     void setPort() {
         RestAssured.port = port;
+    }
+
+    public RequestSpecification baseRestAssuredWithAuth() {
+        return RestAssured.given()
+                .header(AUTHORIZATION, "Bearer " + jwtTokenProvider.createAccessToken(1L))
+                .log()
+                .all()
+                .contentType(ContentType.JSON);
+    }
+
+    public RequestSpecification baseRestAssured() {
+        return RestAssured.given()
+                .log()
+                .all()
+                .contentType(ContentType.JSON);
     }
 }
