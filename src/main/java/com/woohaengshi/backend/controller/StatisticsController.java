8c519cc99484c8f488fb21fdfefaa071bb721593
Kang Hyun Woo <gusdn741021@gmail.com>
2024-08-14 10:33:14 +0900

    [refactor/#8]: 요청 파라미터 타입 에러 수정
     - 에러 코드로 응답주기

--- src/main/java/com/woohaengshi/backend/controller/StatisticsController.java
+++ src/main/java/com/woohaengshi/backend/controller/StatisticsController.java
@@ -3,12 +3,16 @@ package com.woohaengshi.backend.controller;
 import com.woohaengshi.backend.controller.auth.MemberId;
 import com.woohaengshi.backend.domain.statistics.StatisticsType;
 import com.woohaengshi.backend.dto.response.statistics.ShowRankSnapshotResponse;
+import com.woohaengshi.backend.exception.ErrorCode;
+import com.woohaengshi.backend.exception.ErrorResponse;
+import com.woohaengshi.backend.exception.WoohaengshiException;
 import com.woohaengshi.backend.service.statistics.StatisticsService;
 
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,9 +28,15 @@ public class StatisticsController {
     @GetMapping
     public ShowRankSnapshotResponse getRanking(
             @PageableDefault(size = 100) Pageable pageable,
-            @RequestParam(value = "type", defaultValue = "WEEKLY") StatisticsType statisticsType,
+            @RequestParam(value = "type", defaultValue = "WEEKLY") String type,
             @MemberId Long memberId) {
 
-        return statisticsService.showRankData(memberId, statisticsType, pageable);
+        try {
+            StatisticsType statisticsType = StatisticsType.valueOf(type.toUpperCase());
+            return statisticsService.showRankData(memberId, statisticsType, pageable);
+        } catch (IllegalArgumentException e) {
+            throw new WoohaengshiException(ErrorCode.EXIST_STATISTICS_TYPE);
+        }
     }
+
 }
