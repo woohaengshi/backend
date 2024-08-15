d83ac41e5de644ac9c213193b45a1b8ff84ccf3e
Kang Hyun Woo <gusdn741021@gmail.com>
2024-08-15 22:18:29 +0900

    [feat/#62]: 5시 마다 주간 업데이트 기능 추가
     - cron 표현식으로 작성

--- src/main/java/com/woohaengshi/backend/scheduler/StatisticsScheduler.java
+++ src/main/java/com/woohaengshi/backend/scheduler/StatisticsScheduler.java
@@ -2,10 +2,16 @@ package com.woohaengshi.backend.scheduler;
 
 import com.woohaengshi.backend.service.statistics.StatisticsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class StatisticsScheduler {
     private final StatisticsService statisticsService;
+
+    @Scheduled(cron = "0 0 5 * * ?")
+    public void UpdateWeekly() {
+        statisticsService.updateStatisticsWeeklyTime();
+    }
 }
