89bf8e5ee968713b458776b1dd7918fe6e86134a
rlfrkdms1 <rlfrkdms1@naver.com>
2024-08-16 12:49:31 +0900

    [feat/#18]: 주 단위, 월 단위 시간 초기화하는 기능 추가

--- src/main/java/com/woohaengshi/backend/repository/StatisticsRepository.java
+++ src/main/java/com/woohaengshi/backend/repository/StatisticsRepository.java
@@ -8,6 +8,7 @@ import jakarta.persistence.criteria.Predicate;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
@@ -36,4 +37,13 @@ public interface StatisticsRepository
             return query.getRestriction();
         };
     }
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("UPDATE Statistics s set s.weeklyTime = 0")
+    void initWeeklyTime();
+
+
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Statistics s set s.monthlyTime = 0")
+    void initMonthlyTime();
 }
