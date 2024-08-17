e53d40b0d87ebe368c7f8da1328ade96cd56edbb
Kang Hyun Woo <gusdn741021@gmail.com>
2024-08-17 19:20:47 +0900

    [feat/#75]: s3 설정 파일 추가

--- src/main/java/com/woohaengshi/backend/config/S3Config.java
+++ src/main/java/com/woohaengshi/backend/config/S3Config.java
@@ -1,9 +1,37 @@
 package com.woohaengshi.backend.config;
 
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class S3Config {
+    @Value("${cloud.aws.credentials.accessKey}")
+    private String accessKey;
 
+    @Value("${cloud.aws.credentials.secretKey}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+
+    @Bean
+    public BasicAWSCredentials awsCredentials(){
+        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+        return credentials;
+    }
+
+    @Bean
+    public AmazonS3 amazonS3(){
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials()))
+                .build();
+    }
 }
