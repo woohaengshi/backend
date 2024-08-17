059ed58379e1324815e81a8fb48d97a18c5a4404
Kang Hyun Woo <gusdn741021@gmail.com>
2024-08-17 20:21:04 +0900

    [feat/#75]: presigned url 취소

--- src/main/java/com/woohaengshi/backend/config/S3Config.java
+++ src/main/java/com/woohaengshi/backend/config/S3Config.java
@@ -1,14 +1,10 @@
 package com.woohaengshi.backend.config;
 
-
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Getter
 @Configuration
 public class S3Config {
     @Value("${cloud.aws.credentials.accessKey}")
@@ -21,17 +17,4 @@ public class S3Config {
     private String region;
 
 
-    @Bean
-    public BasicAWSCredentials awsCredentials(){
-        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-        return credentials;
-    }
-
-    @Bean
-    public AmazonS3 amazonS3(){
-        return AmazonS3ClientBuilder.standard()
-                .withRegion(region)
-                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials()))
-                .build();
-    }
 }
