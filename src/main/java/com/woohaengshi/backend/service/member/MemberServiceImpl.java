4811a7ad00550b58d8bca2e82b389e22bf66d80a
yaejinkong <0910a@naver.com>
2024-08-19 03:30:58 +0900

    [refactor/#74]: @Transactional(readOnly = true)로 변경

--- src/main/java/com/woohaengshi/backend/service/member/MemberServiceImpl.java
+++ src/main/java/com/woohaengshi/backend/service/member/MemberServiceImpl.java
@@ -11,7 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
+@Transactional(readOnly = true)
 public class MemberServiceImpl implements MemberService {
 
   private final MemberRepository memberRepository;
