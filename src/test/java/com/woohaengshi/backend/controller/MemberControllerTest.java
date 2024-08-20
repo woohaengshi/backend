4382021a0443dd8bf0258ca36b79ea01e944d469
yaejinkong <0910a@naver.com>
2024-08-20 09:07:08 +0900

    [test/#74]: 에노테이션 및 필드 삭제

--- src/test/java/com/woohaengshi/backend/controller/MemberControllerTest.java
+++ src/test/java/com/woohaengshi/backend/controller/MemberControllerTest.java
@@ -11,15 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class MemberControllerTest extends ControllerTest {
-    @LocalServerPort private int port;
-
-    @BeforeEach
-    void setPort() {
-        RestAssured.port = port;
-    }
-
     @Test
     void 회원_정보를_조회한다() {
         baseRestAssuredWithAuth()
