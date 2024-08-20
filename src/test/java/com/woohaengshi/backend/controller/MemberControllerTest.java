b7f9ad5815d4a3b9b09ce7b6d7a690376586325e
rlfrkdms1 <rlfrkdms1@naver.com>
2024-08-20 09:45:26 +0900

    [test/#83]: 비밀번호 형식 다른 경우 컨트롤러 예외 테스트

--- src/test/java/com/woohaengshi/backend/controller/MemberControllerTest.java
+++ src/test/java/com/woohaengshi/backend/controller/MemberControllerTest.java
@@ -4,6 +4,7 @@ import com.woohaengshi.backend.dto.request.member.ChangePasswordRequest;
 import com.woohaengshi.backend.support.ControllerTest;
 import org.junit.jupiter.api.Test;
 
+import static com.woohaengshi.backend.exception.ErrorCode.INVALID_INPUT;
 import static org.springframework.http.HttpStatus.OK;
 
 class MemberControllerTest extends ControllerTest {
@@ -19,6 +20,19 @@ class MemberControllerTest extends ControllerTest {
                 .log()
                 .all()
                 .statusCode(OK.value());
+    }
+
+    @Test
+    void 비밀번호_형식이_다른_경우_예외(){
+        ChangePasswordRequest request = new ChangePasswordRequest("password12", "newPassword!@");
+        baseRestAssuredWithAuth()
+                .body(request)
+                .when()
+                .post("/api/v1/members")
+                .then()
+                .log()
+                .all()
+                .statusCode(INVALID_INPUT.getStatus().value());
     }
 
 }
