159d667f85e4eeea8f563360c183d4d434d821b1
rlfrkdms1 <rlfrkdms1@naver.com>
2024-08-20 09:45:00 +0900

    [feat/#83]: request validation 추가

--- src/main/java/com/woohaengshi/backend/dto/request/member/ChangePasswordRequest.java
+++ src/main/java/com/woohaengshi/backend/dto/request/member/ChangePasswordRequest.java
@@ -1,11 +1,19 @@
 package com.woohaengshi.backend.dto.request.member;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public class ChangePasswordRequest {
 
+    @Pattern(
+            regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*]).{8,20}$",
+            message = "비밀번호는 영어, 숫자, 특수문자를 포함해 최소 8자 이상 최대 20자 이하만 가능합니다.")
     private String oldPassword;
+
+    @Pattern(
+            regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*]).{8,20}$",
+            message = "비밀번호는 영어, 숫자, 특수문자를 포함해 최소 8자 이상 최대 20자 이하만 가능합니다.")
     private String newPassword;
 
     public ChangePasswordRequest(String oldPassword, String newPassword) {
@@ -13,6 +21,5 @@ public class ChangePasswordRequest {
         this.newPassword = newPassword;
     }
 
-    private ChangePasswordRequest() {
-    }
+    private ChangePasswordRequest() {}
 }
