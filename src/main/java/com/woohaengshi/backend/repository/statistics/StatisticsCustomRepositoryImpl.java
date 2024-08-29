3f55d53f7a041e14c51fe27b32550b785b308ea4
Kang Hyun Woo <gusdn741021@gmail.com>
2024-08-29 20:12:46 +0900

    [refactor/#87]: StatisticsCustomRepositoryImpl 정의

---
+++ src/main/java/com/woohaengshi/backend/repository/statistics/StatisticsCustomRepositoryImpl.java
@@ -0,0 +1,10 @@
+package com.woohaengshi.backend.repository.statistics;
+
+
+import lombok.RequiredArgsConstructor;
+
+
+@RequiredArgsConstructor
+public class StatisticsCustomRepositoryImpl implements StatisticsCustomRepository{
+
+}
