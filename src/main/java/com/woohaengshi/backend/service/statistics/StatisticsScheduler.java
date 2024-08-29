2ffb048d0ed986368d9ea6761f06beb4da49a2e6
Kang Hyun Woo <gusdn741021@gmail.com>
2024-08-29 10:36:02 +0900

    [fix]: 주간/월간 초기화 시간 적용

--- src/main/java/com/woohaengshi/backend/service/statistics/StatisticsScheduler.java
+++ src/main/java/com/woohaengshi/backend/service/statistics/StatisticsScheduler.java
@@ -1,5 +1,6 @@
 package com.woohaengshi.backend.service.statistics;
 
+import com.woohaengshi.backend.repository.StatisticsRepository;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.scheduling.annotation.Scheduled;
@@ -8,12 +9,14 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class StatisticsScheduler {
-
-    private final StatisticsService statisticsService;
-
+    private final StatisticsRepository statisticsRepository;
     @Scheduled(cron = "0 0 5 1 * ?", zone = "Asia/Seoul")
-    private void initMonthlyStatistics() {}
+    private void initMonthlyStatistics() {
+        statisticsRepository.initWeeklyTime();
+    }
 
     @Scheduled(cron = "0 0 5 * * MON", zone = "Asia/Seoul")
-    private void initWeeklyStatistics() {}
+    private void initWeeklyStatistics() {
+        statisticsRepository.initMonthlyTime();
+    }
 }
