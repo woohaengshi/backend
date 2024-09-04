b52126a8264f2ccc68eb196b74982bcbd882e8db
rlfrkdms1 <83744709+rlfrkdms1@users.noreply.github.com>
2024-09-04 22:40:29 +0900

    [feat/#92] dto에 필드 추가

--- src/main/java/com/woohaengshi/backend/dto/request/SendMailRequest.java
+++ src/main/java/com/woohaengshi/backend/dto/request/SendMailRequest.java
@@ -9,6 +9,12 @@ public class SendMailRequest {
     @Email(message = "잘못된 이메일 형식입니다.")
     private final String email;
 
+    private final Course course;
+
+    private final name;
+
+    private SendMailRequest(){}
+
     public SendMailRequest(String email) {
         this.email = email;
     }
