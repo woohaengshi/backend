8e451b6aa2e90b359a3fb212f68a331087c0b704
rlfrkdms1 <rlfrkdms1@naver.com>
2024-09-10 14:33:52 +0900

    [refactor/#92]: 문자열 모두 상수로 분리

--- src/main/java/com/woohaengshi/backend/service/PasswordService.java
+++ src/main/java/com/woohaengshi/backend/service/PasswordService.java
@@ -33,6 +33,8 @@ public class PasswordService {
     private final AuthenticationCodeRepository authenticationCodeRepository;
     private static final String WOOHAENGSHI_MAIL = "woohaengshi@gmail.com";
     private static final String CONTENT_FORMAT = "[우행시] 비밀번호 재설정을 위한 임시 번호 발급 안내\n" + "\n" + "안녕하세요, %s님.\n" + "\n" + "비밀번호를 잊으셨나요? 아래의 임시 번호를 사용하여 비밀번호를 재설정할 수 있습니다.\n" + "\n" + "임시 번호: [%s]\n" + "\n" + "이 번호는 30분 동안만 유효하며, 타인과 공유하지 않도록 주의해 주세요. 새로운 비밀번호로 변경하려면 아래의 링크를 클릭하세요.\n" + "\n" + "비밀번호 재설정 링크: [%s]\n" + "\n" + "만약 이 요청을 본인이 하지 않았다면, 즉시 저희에게 알려주세요.\n" + "\n" + "감사합니다.\n" + "우행시 팀 드림\n";
+    private static final String TITLE = "[우행시] 비밀번호 재설정을 위한 임시 번호 발급 안내";
+    private static final String REISSUE_FORMAT = "https://woohangshi.vercel.app/password/%s";
 
     @Transactional(readOnly = true)
     public void sendMail(SendMailRequest request) {
@@ -51,11 +53,11 @@ public class PasswordService {
 
     private MimeMessage createMail(Member member) {
         AuthenticationCode authenticationCode = new AuthenticationCode(member.getId());
-        String reissueLink = "https://woohangshi.vercel.app/password/" + authenticationCode.getCode();
+        String reissueLink = String.format(REISSUE_FORMAT, authenticationCode.getCode());
         MimeMessage mimeMessage = mailSender.createMimeMessage();
         try {
             mimeMessage.setFrom(WOOHAENGSHI_MAIL);
-            mimeMessage.setSubject("[우행시] 비밀번호 재설정을 위한 임시 번호 발급 안내");
+            mimeMessage.setSubject(TITLE);
             mimeMessage.setText(String.format(CONTENT_FORMAT, member.getName(), authenticationCode.getCode(), reissueLink));
             mimeMessage.setRecipients(Message.RecipientType.TO, member.getEmail());
         } catch (MessagingException e) {
