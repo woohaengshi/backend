392e9f98f9f54855f69b689b2287cddaaf3f80e3
rlfrkdms1 <rlfrkdms1@naver.com>
2024-09-10 16:46:36 +0900

    [refactor/#92]: 필요없는 공백 삭제

--- src/main/java/com/woohaengshi/backend/exception/ErrorCode.java
+++ src/main/java/com/woohaengshi/backend/exception/ErrorCode.java
@@ -33,13 +33,13 @@ public enum ErrorCode {
     REFRESH_TOKEN_EXPIRED(UNAUTHORIZED, "리프레시 토큰의 유효기간이 만료되었습니다."),
     SUBJECT_ALREADY_EXISTS(CONFLICT, "이미 존재하는 과목입니다."),
     COURSE_NOT_FOUND(NOT_FOUND, "과정명을 찾을 수 없습니다. "),
-    TIME_HAVE_TO_GREATER_THAN_EXIST(BAD_REQUEST, "요청한 공부 기록은 이전 공부 기록 시간 보다 커야 합니다. "),
+    TIME_HAVE_TO_GREATER_THAN_EXIST(BAD_REQUEST, "요청한 공부 기록은 이전 공부 기록 시간 보다 커야 합니다."),
     EMAIL_ALREADY_EXIST(CONFLICT, "이미 존재하는 이메일입니다."),
     FAILED_SAVE_IMAGE(INTERNAL_SERVER_ERROR, "이미지 저장 실패"),
     QUIT_MEMBER(FORBIDDEN, "탈퇴한 회원입니다."),
     INACTIVE_SUBJECT(BAD_REQUEST, "이미 비활성화된 과목입니다."),
     PASSWORD_INCORRECT(BAD_REQUEST, "맞지 않는 비밀번호입니다."),
-    CREATE_MAIL_EXCEPTION(INTERNAL_SERVER_ERROR, "메일 생성 중 오류가 발생했습니다. "),
+    CREATE_MAIL_EXCEPTION(INTERNAL_SERVER_ERROR, "메일 생성 중 오류가 발생했습니다."),
     INCORRECT_MEMBER_INFO(BAD_REQUEST, "사용자의 정보가 잘못되었습니다."),
     AUTHENTICATE_CODE_NOT_FOUND(NOT_FOUND, "인증 코드를 찾을 수 없습니다.");
 
