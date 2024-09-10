fe10f5fde5e66a72d6d2018030406ff228b65af6
Kang Hyun Woo <gusdn741021@gmail.com>
2024-09-10 09:22:45 +0900

    [feat/#115]: 이미지 변경 api 작성(controller)
     - formdata 요청으로 수정할 post 요청 코드 추가

--- src/main/java/com/woohaengshi/backend/controller/MemberController.java
+++ src/main/java/com/woohaengshi/backend/controller/MemberController.java
@@ -15,13 +15,8 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
@@ -52,4 +47,11 @@ public class MemberController {
                 .header(SET_COOKIE, cookieProvider.createSignOutCookie().toString())
                 .build();
     }
+
+    @PostMapping("/profile")
+    public ResponseEntity<Void> changePassword(@MemberId Long memberId, @RequestPart("profile") MultipartFile profile) {
+        memberService.changeProfile(memberId, profile);
+        return ResponseEntity.ok().build();
+    }
+
 }
