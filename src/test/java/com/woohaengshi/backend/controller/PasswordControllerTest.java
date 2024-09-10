aa9aff9e975c01deebdc3c78d251e6902ec7c8f7
rlfrkdms1 <rlfrkdms1@naver.com>
2024-09-10 15:46:22 +0900

    [test/#92]: 인증코드 검증 기능 테스트

--- src/test/java/com/woohaengshi/backend/controller/PasswordControllerTest.java
+++ src/test/java/com/woohaengshi/backend/controller/PasswordControllerTest.java
@@ -1,5 +1,6 @@
 package com.woohaengshi.backend.controller;
 
+import com.woohaengshi.backend.dto.request.password.ChangePasswordRequest;
 import com.woohaengshi.backend.dto.request.password.SendMailRequest;
 import com.woohaengshi.backend.support.ControllerTest;
 import org.junit.jupiter.api.Test;
@@ -7,6 +8,8 @@ import org.junit.jupiter.api.Test;
 import static org.springframework.http.HttpStatus.OK;
 
 class PasswordControllerTest extends ControllerTest {
+
+    private String authenticateCode = "a1f3ac33-bfa5-4c0d-aede-aed68609558d";
 
     @Test
     void 비밀번호_재설정_메일을_보낼_수_있다() {
@@ -20,5 +23,17 @@ class PasswordControllerTest extends ControllerTest {
                 .all()
                 .statusCode(OK.value());
     }
+
+    @Test
+    void 인증코드를_검증_할_수_있다() {
+        baseRestAssured()
+                .when()
+                .get("/api/v1/password/" + authenticateCode)
+                .then()
+                .log()
+                .all()
+                .statusCode(OK.value());
+    }
+    
 
 }
