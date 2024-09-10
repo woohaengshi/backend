fb624a6a607fef58648d5c96ea5f8fae656f9723
rlfrkdms1 <rlfrkdms1@naver.com>
2024-09-10 15:38:29 +0900

    [feat/#92]: 사용하지 않는 메서드 삭제

--- src/main/java/com/woohaengshi/backend/repository/AuthenticationCodeRepository.java
+++ src/main/java/com/woohaengshi/backend/repository/AuthenticationCodeRepository.java
@@ -3,10 +3,5 @@ package com.woohaengshi.backend.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface AuthenticationCodeRepository extends CrudRepository<AuthenticationCode, String> {
-    Optional<AuthenticationCode> findByCode(String code);
-    boolean existsByCode(String code);
-}
+public interface AuthenticationCodeRepository extends CrudRepository<AuthenticationCode, String> {}
