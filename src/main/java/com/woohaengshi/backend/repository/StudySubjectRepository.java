9773a9a6bc6326511ed14c4218468e6ed576bbcf
qbobl5 <ddddd991125@gmail.com>
2024-09-11 11:24:24 +0900

    [feat/#120]: 필요없는 메소드 제거

--- src/main/java/com/woohaengshi/backend/repository/StudySubjectRepository.java
+++ src/main/java/com/woohaengshi/backend/repository/StudySubjectRepository.java
@@ -4,6 +4,9 @@ import com.woohaengshi.backend.domain.StudySubject;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 public interface StudySubjectRepository extends JpaRepository<StudySubject, Long> {
     boolean existsBySubjectIdAndStudyRecordId(Long subjectId, Long studyRecordId);
 }
