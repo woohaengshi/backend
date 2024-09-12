1966bcf7a6e0823530cf880d7f110efd76fa7f68
Kang Hyun Woo <gusdn741021@gmail.com>
2024-09-12 21:53:16 +0900

    [feat/#124]: 주간 시간 로그 저장

--- src/main/java/com/woohaengshi/backend/service/statistics/StatisticsScheduler.java
+++ src/main/java/com/woohaengshi/backend/service/statistics/StatisticsScheduler.java
@@ -1,5 +1,6 @@
 package com.woohaengshi.backend.service.statistics;
 
+import com.woohaengshi.backend.log.StatisticsLog;
 import com.woohaengshi.backend.repository.StatisticsRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -12,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class StatisticsScheduler {
     private final StatisticsRepository statisticsRepository;
+    private final StatisticsLog statisticsLog;
 
     @Scheduled(cron = "0 0 5 1 * ?", zone = "Asia/Seoul")
     @Transactional
@@ -22,6 +24,7 @@ public class StatisticsScheduler {
     @Scheduled(cron = "0 0 5 * * MON", zone = "Asia/Seoul")
     @Transactional
     public void initWeeklyStatistics() {
+        statisticsLog.printWeeklyRanking();
         statisticsRepository.initWeeklyTime();
     }
 }
