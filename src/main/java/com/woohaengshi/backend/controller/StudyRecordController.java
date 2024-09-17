b2487eae959621fdeb8a048d0d35eeda47d6554a
yaejinkong <0910a@naver.com>
2024-09-17 23:53:42 +0900

    [feat/#128]: controller 추가

--- src/main/java/com/woohaengshi/backend/controller/StudyRecordController.java
+++ src/main/java/com/woohaengshi/backend/controller/StudyRecordController.java
@@ -1,6 +1,7 @@
 package com.woohaengshi.backend.controller;
 
 import com.woohaengshi.backend.controller.auth.MemberId;
+import com.woohaengshi.backend.dto.request.studyrecord.EditSubjectAndCommentRequest;
 import com.woohaengshi.backend.dto.request.studyrecord.SaveCommentRequest;
 import com.woohaengshi.backend.dto.request.studyrecord.SaveRecordRequest;
 import com.woohaengshi.backend.dto.response.studyrecord.ShowMonthlyRecordResponse;
@@ -50,6 +51,13 @@ public class StudyRecordController {
     public ResponseEntity<Void> saveComment(
             @Valid @RequestBody SaveCommentRequest request, @MemberId Long memberId) {
         studyRecordService.saveComment(request, memberId);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/edit")
+    public ResponseEntity<Void> editSubjectsAndComment(
+            @Valid @RequestBody EditSubjectAndCommentRequest request, @MemberId Long memberId) {
+        studyRecordService.editSubjectsAndComment(request, memberId);
         return ResponseEntity.ok().build();
     }
 }
