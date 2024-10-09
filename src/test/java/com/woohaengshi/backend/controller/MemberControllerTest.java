d04e53ef02fa1b48eb197751591edbc7a0fa73c2
yaejinkong <0910a@naver.com>
2024-10-09 23:37:20 +0900

    [test/#130]: 회원 정보를 수정하는 컨트롤러 테스트 추가

--- src/test/java/com/woohaengshi/backend/controller/MemberControllerTest.java
+++ src/test/java/com/woohaengshi/backend/controller/MemberControllerTest.java
@@ -6,6 +6,7 @@ import static org.springframework.http.HttpStatus.NO_CONTENT;
 import static org.springframework.http.HttpStatus.OK;
 
 import com.woohaengshi.backend.dto.request.member.ChangePasswordRequest;
+import com.woohaengshi.backend.dto.request.member.MemberRequest;
 import com.woohaengshi.backend.support.ControllerTest;
 
 import org.junit.jupiter.api.Test;
@@ -59,5 +60,18 @@ class MemberControllerTest extends ControllerTest {
                 .log()
                 .all()
                 .statusCode(NO_CONTENT.value());
+    }
+
+    @Test
+    void 회원_정보를_수정할_수_있다() {
+        MemberRequest request = new MemberRequest("김수정", "클라우드 엔지니어링");
+        baseRestAssuredWithAuth()
+                .body(request)
+                .when()
+                .patch("/api/v1/members")
+                .then()
+                .log()
+                .all()
+                .statusCode(OK.value());
     }
 }
